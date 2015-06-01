bf513c81b235290c570e236d5f16481452f90018
Zack Boehm <git@zackboehm.com>
2015-06-01 12:35:21 -0400

    jshint

--- src/bg/background.js
+++ src/bg/background.js
@@ -2,10 +2,10 @@ chrome.webRequest.onHeadersReceived.addListener(function(details){
   for(var o in details.responseHeaders){
 
     // Massdrop responded with a Location header
-    if(details.responseHeaders[o]["name"] === "Location"){
+    if(details.responseHeaders[o].name === "Location"){
 
       // And it isn't pointing to what we asked for
-      if(details.responseHeaders[o]["value"] != details.url){
+      if(details.responseHeaders[o].value != details.url){
 
         // Add guest_open to the url and update the tab
         var fixedUrl = details.url+'?mode=guest_open';
